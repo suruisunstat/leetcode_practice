5d2c0a6bf545e575ba03048a6f2d2045fc967d1e
suruisunstat <tonysun2015@gmail.com>
2018-09-03 09:45:53 -0700

    Update longestConsecutive.java

--- longestConsecutive.java
+++ longestConsecutive.java
@@ -6,23 +6,23 @@ Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefor
 
 class Solution {
     public int longestConsecutive(int[] nums) {
-    int longestLength = 0;
-		Set<Integer> map = new HashSet<Integer>();
-		for (int i = 0; i < nums.length; i++) {
-			  map.add(nums[i]);
-		}
+    	int longestLength = 0;
+    	Set<Integer> map = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+	    map.add(nums[i]);
+    	}
 		
-		for (int i = 0; i < nums.length; i++) {
-			int curLength = 0;
-			int cur = nums[i];
-            if (!map.contains(cur-1)) {
-                while (map.contains(cur)) {
-				curLength++;
-				cur += 1;
-			    }
-            }
-            longestLength = Math.max(curLength, longestLength);
-		}
-		return longestLength;
+    	for (int i = 0; i < nums.length; i++) {
+	    int curLength = 0;
+	    int cur = nums[i];
+	    if (!map.contains(cur-1)) {
+	    	while (map.contains(cur)) {
+		    curLength++;
+		    cur += 1;
+	    	}
+	    }
+    	longestLength = Math.max(curLength, longestLength);
+    	}
+    return longestLength;
     }
 }
