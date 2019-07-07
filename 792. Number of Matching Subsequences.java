c7ca563eca581c55664c432312d5f91f42a34e02
suruisunstat <tonysun2015@gmail.com>
2019-07-06 22:40:26 -0700

    Update 792. Number of Matching Subsequences.java

--- 792. Number of Matching Subsequences.java
+++ 792. Number of Matching Subsequences.java
@@ -32,3 +32,71 @@ class Solution {
         return res;
     }
 }
+
+
+### Python
+
+## first code TLE, second code almost TLE but still pass
+class Solution(object):
+    def numMatchingSubseq(self, S, words):
+        """
+        :type S: str
+        :type words: List[str]
+        :rtype: int
+        """
+        dict = {}
+        for i,s in enumerate(S):
+            if s not in dict:
+                dict[s] = [i]
+            else:
+                dict[s].append(i)
+
+        res = 0
+        for word in words:
+            d = 0
+            k = -1
+            for d in range(len(word)):
+                if word[d] not in dict:
+                    break
+                else:
+                    l = dict[word[d]]
+                    has = False
+                    for m in range(len(l)):
+                        if l[m] > k:
+                            k = l[m]
+                            has = True
+                            break
+                    if not has:
+                        break
+            #print(d)
+            if d == len(word) - 1 and has:
+                res += 1
+        return res
+        
+        
+#         def isSubsequence(s,t):
+#             if len(s) == 0:
+#                 return True
+#             idx = 0
+#             for tt in t:
+#                 if s[idx] == tt:
+#                     idx += 1
+#                 if idx == len(s):
+#                     return True
+#             return False
+        
+#         count = 0
+#         passSet = set()
+#         failSet = set()
+#         for word in words:
+#             if word in passSet or word in failSet:
+#                 if word in passSet:
+#                     count += 1
+#                 continue
+            
+#             if isSubsequence(word,S):
+#                 count += 1
+#                 passSet.add(word)
+#             else:
+#                 failSet.add(word)
+#         return count
