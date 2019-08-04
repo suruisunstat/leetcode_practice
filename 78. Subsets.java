79f5df12fa4038b9d1a743b5bede356b96df0cc0
suruisunstat <tonysun2015@gmail.com>
2019-08-04 16:16:25 -0700

    Update 78. Subsets.java

--- 78. Subsets.java
+++ 78. Subsets.java
@@ -1,22 +1,39 @@
 class Solution {
-    private List<List<Integer>> res = new ArrayList<>();
+    //private List<List<Integer>> res = new ArrayList<>();
+    
+    // public List<List<Integer>> subsets(int[] nums) {
+    //     List<List<Integer>> res = new ArrayList<>();
+    //     //res.add(new ArrayList<>());
+    //     List<Integer> temp = new ArrayList<>();
+    //     dfs(res, temp, nums, 0);
+    //     return res;
+    // }
+    
+    // public void dfs(List<List<Integer>> res, List<Integer> temp, int[] nums, int j) {
+    //     res.add(new ArrayList<Integer>(temp)); // essential to add a copy of temp arraylist
+    //     for (int i = j; i < nums.length; i++) {
+    //         temp.add(nums[i]);
+    //         dfs(res, temp, nums, i+1);
+    //         temp.remove(temp.size() - 1);
+    //     }
+    // }
     
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        //res.add(new ArrayList<>());
-        List<Integer> temp = new ArrayList<>();
-        dfs(res, temp, nums, 0);
+        res.add(new ArrayList<>());
+        for (int num : nums) {
+            int size = res.size();
+            for(int i = 0; i < size; i++) {
+                List<Integer> temp = new ArrayList<>(res.get(i)); // this step is essential because otherwise the value in res will be updated
+                //List<Integer> temp = res.get(i); 
+                temp.add(num);
+                res.add(temp);
+            }
+            //System.out.println(res.size());
+        }
         return res;
     }
     
-    public void dfs(List<List<Integer>> res, List<Integer> temp, int[] nums, int j) {
-        res.add(new ArrayList<Integer>(temp));
-        for (int i = j; i < nums.length; i++) {
-            temp.add(nums[i]);
-            dfs(res, temp, nums, i+1);
-            temp.remove(temp.size() - 1);
-        }
-    }  
+// O(2^n)    
 }
 
-O(2^n) ? 
