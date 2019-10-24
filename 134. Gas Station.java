05c13a6fbba5b1f630579b605c192fac66b4bb32
suruisunstat <tonysun2015@gmail.com>
2019-10-24 12:28:09 -0700

    Update 134. Gas Station.java

--- 134. Gas Station.java
+++ 134. Gas Station.java
@@ -29,4 +29,24 @@ class Solution {
         }
         return -1;
     }
+    
+        public int canCompleteCircuit(int[] gas, int[] cost) {
+        if (gas == null || cost == null || gas.length == 0 || cost.length == 0 || gas.length != cost.length) {return -1;}
+        
+        int sum = 0;
+        int total = 0;
+        int index = 0;
+        for (int i = 0; i < gas.length; i++) {
+            sum += gas[i] - cost[i];
+            total += gas[i] - cost[i];
+            
+            if (sum < 0) {
+                sum = 0;
+                index = i + 1;
+            }
+        }
+        
+        if (total < 0) return -1;
+        return index;
+    }
 }
