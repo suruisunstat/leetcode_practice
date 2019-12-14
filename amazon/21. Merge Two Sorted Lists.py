88359d886a4493d7ae6f64b4ce42f9634f983446
suruisunstat <tonysun2015@gmail.com>
2019-12-14 12:44:55 -0800

    Update 21. Merge Two Sorted Lists.py

--- amazon/21. Merge Two Sorted Lists.py
+++ amazon/21. Merge Two Sorted Lists.py
@@ -37,3 +37,18 @@ class Solution(object):
         
         return head.next
             
+        
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        # maintain an unchanging reference to node ahead of the return node.
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next,l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1,l2.next)
+            return l2
