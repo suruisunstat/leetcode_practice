da37748f1d4182a14c1a4304f79661101c96aa8e
suruisunstat <tonysun2015@gmail.com>
2020-01-05 19:56:16 -0800

    Create 253. Meeting Rooms II.py

---
+++ amazon/python/253. Meeting Rooms II.py
@@ -0,0 +1,23 @@
+import heapq
+
+class Solution(object):
+    def minMeetingRooms(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        # [[0, 30],[5, 10],[15, 20]]
+        
+        if len(intervals) == 0:
+            return 0
+        
+        intervals = sorted(intervals, key = lambda x: x[0])
+        free_rooms = []
+        heapq.heappush(free_rooms,intervals[0][1])
+        
+        for i in range(1,len(intervals)):
+            if free_rooms[0] <= intervals[i][0]:
+                heapq.heappop(free_rooms)
+            heapq.heappush(free_rooms, intervals[i][1])
+            
+        return len(free_rooms)
