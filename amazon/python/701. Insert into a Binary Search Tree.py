1dc342ba0ad752b13796f6b8658c90cc34b33efb
suruisunstat <tonysun2015@gmail.com>
2020-04-19 11:07:18 -0700

    Create 701. Insert into a Binary Search Tree.py

---
+++ amazon/python/701. Insert into a Binary Search Tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def insertIntoBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if root is None:
+            return TreeNode(val)
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        elif val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        return root 
+        
+        # Time: O(logn)
+        # Space: O(logn)
