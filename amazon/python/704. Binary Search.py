35f627d062f0ff227ea0450124c993b51a1bd875
suruisunstat <tonysun2015@gmail.com>
2020-04-25 10:48:38 -0700

    Create 704. Binary Search.py

---
+++ amazon/python/704. Binary Search.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l = 0
+        r = len(nums)
+        
+        while l < r:
+            mid = l + (r - l) // 2
+            if target == nums[mid]:
+                return mid
+            elif target > nums[mid]:
+                l = mid + 1
+            else:
+                r = mid
+        return -1
+    
+    # Time: O(logn)
+    # Space: O(1)
